7f7e3d5ca3c0836f3ac5a37b93ef88985f8eec14
hakan.basturknewinn.com <forxynet@gmail.com>
2025-01-26 18:59:15 +0300

    complated

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -25,7 +25,7 @@ const Hero = () => {
             <div className='text-lightOrange mt-[100px] md:mt-0 p-4 space-y-28'>
               <motion.h1
                 initial={{ opacity: 0, y: -100 }}
-                animate={{ opacity: 1, y: 0 }}
+                whileInView={{ opacity: 1, y: 0 }}
                 transition={{
                   type: "sprint",
                   stiffness: 100,
@@ -48,16 +48,17 @@ const Hero = () => {
                   <h1 className='text-2xl'>Black Lifestyle Lovers,</h1>
                   <h1 className='text-sm opacity-55 leading-loose'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, laboriosam explicabo tenetur voluptatibus eius veniam illum. Fugit, rem nam autem corporis nemo vel sapiente laudantium aperiam, perferendis corrupti accusamus? Sapiente. </h1>
                 </div>
-                <div className='absolute -top-6 -left-10 w-[250px] h-[180px] bg-gray-700/25'></div>
+                <div className='absolute -top-6 -left-10 w-[250px] h-[190px] bg-gray-700/25'></div>
               </motion.div>
             </div>
-            {/* Hero Image content section */}
+            {/* Hero Image section */}
             <div className='relative'>
               <motion.img
                 initial={{ opacity: 0, scale: 0 }}
-                animate={{ opacity: 1, scale: 1 }}
+                whileInView={{ opacity: 1, scale: 1 }}
+                viewport={{ one: true }}
                 transition={{
-                  type: "sprint",
+                  type: "spring",
                   stiffness: 100,
                   damping: 10,
                   delay: 0.4,
@@ -68,9 +69,10 @@ const Hero = () => {
               {/* orange circle ring */}
               <motion.div
                 initial={{ opacity: 0, y: 100 }}
-                animate={{ opacity: 1, y: 0 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                viewport={{ one: true }}
                 transition={{
-                  type: "sprint",
+                  type: "spring",
                   stiffness: 100,
                   damping: 10,
                   delay: 0.8,
@@ -79,9 +81,10 @@ const Hero = () => {
               {/* big text section */}
               <motion.div
                 initial={{ opacity: 0, x: -100 }}
-                animate={{ opacity: 1, x: 0 }}
+                whileInView={{ opacity: 1, x: 0 }}
+                viewport={{ one: true }}
                 transition={{
-                  type: "sprint",
+                  type: "spring",
                   stiffness: 100,
                   damping: 10,
                   delay: 1.2,
@@ -93,35 +96,38 @@ const Hero = () => {
               </motion.div>
             </div>
             {/* third div section */}
-            <motion.div
-              initial={{ opacity: 0, scale: 0 }}
-              animate={{ opacity: 1, scale: 1 }}
-              transition={{
-                type: "sprint",
-                stiffness: 100,
-                damping: 10,
-                delay: 0.4,
-              }}
-              className='text-lightOrange mt-[100px] md:mt-0 p-4 space-y-28'>
-              <h1 className='opacity-0 text-7xl font-bold leading-tight ml-14'>Blvck Tumber</h1>
-              <div className='relative'>
-                <div className='relative z-10 space-y-4'>
-                  <h1 className='text-2xl'>Blvck Thumbler</h1>
-                  <h1 className='text-sm opacity-55 leading-loose'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem libero numquam, officiis porro dolor dolores magni earum saepe, quas aliquam dignissimos. </h1>
+            <div className='hidden lg:block'>
+              <motion.div
+                initial={{ opacity: 0, scale: 0 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{
+                  type: "sprint",
+                  stiffness: 100,
+                  damping: 10,
+                  delay: 0.4,
+                }}
+                className='text-lightOrange mt-[100px] md:mt-0 p-4 space-y-28'>
+                <h1 className='opacity-0 text-7xl font-bold leading-tight ml-14'>Blvck Tumber</h1>
+                <div className='relative'>
+                  <div className='relative z-10 space-y-4'>
+                    <h1 className='text-2xl'>Blvck Thumbler</h1>
+                    <h1 className='text-sm opacity-55 leading-loose'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem libero numquam, officiis porro dolor dolores magni earum saepe, quas aliquam dignissimos. </h1>
+                  </div>
+                  <div className='absolute -top-6 -right-10 w-[250px] h-[190px] bg-darkGray/50'></div>
                 </div>
-                <div className='absolute -top-6 -right-10 w-[250px] h-[190px] bg-darkGray/50'></div>
-              </div>
-            </motion.div>
-            <div></div>
+              </motion.div>
+              <div></div>
+            </div>
           </div>
         </div>
+
         {/* sidebar Menu Section */}
         {
           sidebar && (
             <motion.div
               initial={{ z: "100%" }}
               whileInView={{ x: 0 }}
-              className='absolute top-0 right-0 w-[140px] h-full bg-gradient-to-b from-primary/80 to-primaryDark/80 backdrop-blur-sm z-10'>
+              className='absolute top-0 right-0 w-[140px] h-full bg-gradient-to-b from-primary/80 to-primaryDark/80 backdrop-blur-sm z-50'>
               <div className='w-full h-full flex justify-center items-center'>
                 <div className='flex flex-col justify-center items-center gap-6 text-white'>
                   {/* line */}
